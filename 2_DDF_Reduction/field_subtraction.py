1276dfa8e6846730c9ce1a93e0451da7bb6682a5
HrishikeshShetgaonkar <hrishikesh036@gmail.com>
2023-01-18 12:39:52 +0530

    Commented MaskDicoModel as not present in py3.10

--- 2_DDF_Reduction/field_subtraction.py
+++ 2_DDF_Reduction/field_subtraction.py
@@ -5,9 +5,12 @@ from casacore.tables import table
 
 start = time.time()
 
-LastSelfCal_Pass = "p3"
+LastSelfCal_Pass = "p2"
 region_file = "oj287_1800arcsec.reg"
 TargetName = "OJ287"
+NewColName = "FIELD_SUBTRACTED_DATA_FullBand"
+CorrectedDataColName = "CORRECTED_DATA"
+WidefieldDataColName = "WIDEFIELD_DATA"
 
 ms_list =["L723586_121MHz_uv_pre-cal.ms","L723586_123MHz_uv_pre-cal.ms",
 "L723586_125MHz_uv_pre-cal.ms","L723586_127MHz_uv_pre-cal.ms",
@@ -22,7 +25,7 @@ ms_list =["L723586_121MHz_uv_pre-cal.ms","L723586_123MHz_uv_pre-cal.ms",
 "L723586_160MHz_uv_pre-cal.ms","L723586_162MHz_uv_pre-cal.ms",
 "L723586_164MHz_uv_pre-cal.ms","L723586_166MHz_uv_pre-cal.ms"]
 
-DDF = "python /scratch/WORK/ebonnassieux/DDFpy3.6/sources/DDFacet/MyDDF/bin/DDF.py"
+DDF = "python /scratch/WORK/P6_WORKDIR/DDFpy3.10NenuBeam/DDFpy3.10/sources/DDFacet/MyDDF/bin/DDF.py"
 KMS = "python /scratch/WORK/ebonnassieux/DDFpy3.6/sources/killMS/MykMS/bin/kMS.py"
 ClipCal = "python /scratch/WORK/ebonnassieux/DDFpy3.6/sources/killMS/MykMS/lib/python3.6/site-packages/killMS/ClipCal.py"
 Breizorro = "/scratch/WORK/ebonnassieux/DDFpy3.6/bin/breizorro"
@@ -32,10 +35,11 @@ chunks = [ms_list[x:x+8] for x in range(0,len(ms_list), 8)]
 
 def getDDFPredict():
         return f"{DDF} --Data-MS ms_list.txt --Parallel-NCPU 46 " \
-        f"--Image-NPix=7000 --Image-Cell 5 --Weight-Mode=Natural --Output-Also all  --Selection-UVRangeKm=0.1,30 " \
-        f"--Data-ColName=CORRECTED_DATA  --Predict-ColName=WIDEFIELD_DATA --RIME-DecorrMode=FT " \
+        f"--Image-NPix=7000 --Image-Cell 5 --Weight-Mode=Natural --Output-Also all --Selection-UVRangeKm=0.1,30 " \
+        f"--Data-ColName={CorrectedDataColName}  --Predict-ColName={WidefieldDataColName} --RIME-DecorrMode=FT " \
         f"--Facets-NFacets=11 --HMP-MajorStallThreshold=0.2 --HMP-Scales=[0,2,4,8,16,32] " \
-        f"--Weight-ColName=IMAGING_WEIGHT --Cache-Reset=1 --Output-Mode Predict --Predict-InitDicoModel No{TargetName}.DicoModel "
+        f"--Weight-ColName=IMAGING_WEIGHT --Cache-Reset=1 --Output-Mode Predict --Predict-InitDicoModel No{TargetName}.DicoModel " \
+        f"--Deconv-Mode=SSD --GAClean-NSourceKin=200 --GAClean-NMaxGen=150 --Deconv-MaxMajorIter=5 --Mask-Auto=True --Mask-SigTh=10 --Freq-NBand=6 "
 
 def getBreizorroCommand():
         return f"{Breizorro} -r ddf_fullband_{LastSelfCal_Pass}.restored.fits -o oj287_masked.fits --subtract {region_file} -t -99999"
@@ -53,15 +57,15 @@ def waitingAnimation(process):
 
 def subtractVisibilities(ms_name):
         ms = table(ms_name, readonly=False)
-        name = "FIELD_SUBTRACTED_DATA"
+        name = NewColName
         comment = "data after subtracting modeled visibilities for sources in the field other then target source"
 
-        desc = ms.getcoldesc("CORRECTED_DATA")
+        desc = ms.getcoldesc(CorrectedDataColName)
         desc["name"] = name
         desc["comment"] = comment
 
         ms.addcols(desc)
-        ms.putcol(name, ms.getcol("CORRECTED_DATA") - ms.getcol("WIDEFIELD_DATA"))
+        ms.putcol(name, ms.getcol(CorrectedDataColName) - ms.getcol(WidefieldDataColName))
 
         ms.close()
 
@@ -69,46 +73,34 @@ print(f"starting field subtraction for:\n {ms_list[0][0:7]}")
 
 #first subtract oj287 from field
 with open(f"Breizorro_{LastSelfCal_Pass}.log", "w+") as f:
-    process1 = subprocess.Popen(getBreizorroCommand(), shell=True, stdout=f)
+   process1 = subprocess.Popen(getBreizorroCommand(), shell=True, stdout=f)
 print("Breizorro source subtraction running")
 waitingAnimation(process1)
 process1.wait()
 print("Breizorro source subtraction from field done")
 
-#MaskDicoModel
-with open(f"MaskDicoModel_{LastSelfCal_Pass}.log", "w+") as f:
-    process2 = subprocess.Popen(getMaskDicoModelCommand(), shell=True, stdout=f)
-print("MaskDicoModel running")
-waitingAnimation(process2)
-process2.wait()
-print("MaskDicoModel done")
+# #MaskDicoModel
+# with open(f"MaskDicoModel_{LastSelfCal_Pass}.log", "w+") as f:
+#    process2 = subprocess.Popen(getMaskDicoModelCommand(), shell=True, stdout=f)
+# print("MaskDicoModel running")
+# waitingAnimation(process2)
+# process2.wait()
+# print("MaskDicoModel done")
 
-#DDF Predict
-with open(f"DDFPredict_{LastSelfCal_Pass}.log", "w+") as f:
-    process3 = subprocess.Popen(getDDFPredict(), shell=True, stdout=f)
-print("DDFPredict running")
-waitingAnimation(process3)
-process3.wait()
-print("DDFPredict done")
-
-
-#Subtract modeled visibilities of field from the observation visibilities data
-for ms in ms_list:
-        print(f"Subtraction of modeled visibilities running for: {ms}")
-        subtractVisibilities(ms)
-        print(f"Subtraction of modeled visibilities done for: {ms}")
-
-print("COMPLETED field subtraction for all data")
-print('It took', time.time()-start, 'seconds.')
-
-## > breizorro -r ddf_154MH_p3.restored.fits -o oj287_masked.fits --subtract oj287_1800arcsec.reg -t -99999
-## > MaskDicoModel --InDicoModel=ddf_154MH_p3.DicoModel --OutDicoModel=NoOJ287.DicoModel --MaskName=oj287_masked.fits
+# #DDF Predict
+# with open(f"DDFPredict_{LastSelfCal_Pass}.log", "w+") as f:
+#   process3 = subprocess.Popen(getDDFPredict(), shell=True, stdout=f)
+# print("DDFPredict running")
+# waitingAnimation(process3)
+# process3.wait()
+# print("DDFPredict done")
 
 
-# DDF --Data-MS L723586_154MHz_uv_pre-cal_averaged.ms --Parallel-NCPU 46 --Image-NPix=28000 --Image-Cell 1.25 --Weight-Mode=Natural --Output-Also all --Data-ColName=CORRECTED_DATA  --Predict-ColName=WIDEFIELD_DATA --RIME-DecorrMode=FT --Facets-NFacets=11 --HMP-MajorStallThreshold=0.2 --HMP-Scales=[0,2,4,8,16,32] --Weight-ColName=IMAGING_WEIGHT --Output-Mode Predict --Predict-InitDicoModel NoOJ287.DicoModel --Cache-Reset=1
+# #Subtract modeled visibilities of field from the observation visibilities data
+# for ms in ms_list:
+#         print(f"Subtraction of modeled visibilities running for: {ms}")
+#         subtractVisibilities(ms)
+#         print(f"Subtraction of modeled visibilities done for: {ms}")
 
-# DDF --Data-MS L723586_154MHz_uv_pre-cal_averaged.ms --Parallel-NCPU 46 
-# --Image-NPix=7000 --Image-Cell 5 --Weight-Mode=Natural --Output-Also all 
-# --Data-ColName=CORRECTED_DATA  --Predict-ColName=WIDEFIELD_DATA --RIME-DecorrMode=FT 
-# --Facets-NFacets=11 --HMP-MajorStallThreshold=0.2 --HMP-Scales=[0,2,4,8,16,32] 
-# --Weight-ColName=IMAGING_WEIGHT --Cache-Reset=1 --Output-Mode Predict --Predict-InitDicoModel NoOJ287.DicoModel 
+# print("COMPLETED field subtraction for all data")
+# print('It took', time.time()-start, 'seconds.')
